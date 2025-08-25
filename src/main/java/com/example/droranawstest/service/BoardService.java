c08d508ac083b9e471b0845678aebbe717db3853
won2dev-backand <seodongwoncp@gmail.com>
2025-08-25 23:27:14 +0900

    init commit

--- src/main/java/com/example/droranawstest/service/BoardService.java
+++ src/main/java/com/example/droranawstest/service/BoardService.java
@@ -4,6 +4,7 @@ import com.example.droranawstest.controller.BoardResponse;
 import com.example.droranawstest.domain.Board;
 import com.example.droranawstest.domain.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    @Cacheable(cacheNames = "board", key = "#id")
     public BoardResponse getBoardById(Long id) {
         Board board = boardRepository.findById(id).orElseThrow();
 
